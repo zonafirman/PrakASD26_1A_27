faf8f22faf6180e371a681f2be815033844dd4a3
jonafirman <jonafirmanmaulana01@gmail.com>
2026-02-25 09:58:07 +0700

    P2_Percobaan2_27

---
+++ src/P3/Percobaan1/Mahasiswa27.java
@@ -0,0 +1,15 @@
+package P3.Percobaan1;
+public class Mahasiswa27 {
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    public void cetakInfo() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("-----------------------------------");
+    }
+}
