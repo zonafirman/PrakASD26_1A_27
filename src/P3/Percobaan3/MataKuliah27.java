28e2dac7c42314a6c8e76995b20c7ea593f7e590
jonafirman <jonafirmanmaulana01@gmail.com>
2026-02-25 10:27:03 +0700

    P3_Percobaan3_27

---
+++ src/P3/Percobaan3/MataKuliah27.java
@@ -0,0 +1,15 @@
+package P3.Percobaan3;
+
+public class MataKuliah27 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public MataKuliah27(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+}
+}
---
+++ src/P3/Percobaan3/MataKuliahDemo27.java
@@ -0,0 +1,38 @@
+package P3.Percobaan3;
+
+import java.util.Scanner;
+
+public class MataKuliahDemo27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MataKuliah27[] arrayOfMatakuliah = new MataKuliah27[3];
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode       : ");
+            kode = sc.nextLine();
+            System.out.print("Nama       : ");
+            nama = sc.nextLine();
+            System.out.print("Sks        : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("-----------------------------------");
+
+            arrayOfMatakuliah[i] = new MataKuliah27(kode, nama, sks, jumlahJam);
+        }
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMatakuliah[i].kode);
+            System.out.println("Nama       : " + arrayOfMatakuliah[i].nama);
+            System.out.println("Sks        : " + arrayOfMatakuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMatakuliah[i].jumlahJam);
+            System.out.println("-----------------------------------");
+        }
+    }
+}
