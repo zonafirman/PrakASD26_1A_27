4b8120922bbc8783317a71599cf7b6a9c5b0d1dd
jonafirman <jonafirmanmaulana01@gmail.com>
2026-03-16 19:46:18 +0700

    P5_TugasB_27

--- src/P5/Tugas/uts27.java
+++ src/P5/Tugas/uts27.java
@@ -11,4 +11,14 @@ public class uts27 {
         int maxKanan = cariMaxDC(arr, tengah + 1, kanan);
         return Math.max(maxKiri, maxKanan);
     }
+
+    public static int cariMinDC(int[] arr, int kiri, int kanan) {
+        if (kiri == kanan) {
+            return arr[kiri];
+        }
+        int tengah = (kiri + kanan) / 2;
+        int minKiri = cariMinDC(arr, kiri, tengah);
+        int minKanan = cariMinDC(arr, tengah + 1, kanan);
+        return Math.min(minKiri, minKanan);
+    }
 }
--- src/P5/Tugas/uts27main.java
+++ src/P5/Tugas/uts27main.java
@@ -5,8 +5,10 @@ public class uts27main {
         int[] nilaiUTS = {78, 85, 90, 76, 92, 88, 80, 82};
         int[] nilaiUAS = {82, 88, 87, 79, 95, 85, 83, 84};
         int maxUTS = uts27.cariMaxDC(nilaiUTS, 0, nilaiUTS.length - 1);
+        int minUTS = uts27.cariMinDC(nilaiUTS, 0, nilaiUTS.length - 1);
        
         System.out.println("=== Hasil Latihan Praktikum ===");
         System.out.println("Nilai UTS tertinggi : " + maxUTS);
+        System.out.println("Nilai UTS terendah : " + minUTS);
     }
 }
