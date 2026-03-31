082a898fcb34dcc19a964c3d524c6e0ba8e8b3fa
jonafirman <jonafirmanmaulana01@gmail.com>
2026-03-31 21:42:16 +0700

    P6_Percobaan2_1_Pertanyaan_27

--- src/P6/MahasiswaDemo27.java
+++ src/P6/MahasiswaDemo27.java
@@ -1,20 +1,26 @@
 package P6;
+import java.util.Scanner;
 
 public class MahasiswaDemo27 {
     public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
         MahasiswaBerprestasi27 list = new MahasiswaBerprestasi27();
-        
-        Mahasiswa27 m1 = new Mahasiswa27("123", "Zidan", "2A", 3.2);
-        Mahasiswa27 m2 = new Mahasiswa27("124", "Ayu", "2A", 3.5);
-        Mahasiswa27 m3 = new Mahasiswa27("125", "Sofi", "2A", 3.1);
-        Mahasiswa27 m4 = new Mahasiswa27("126", "Sita", "2A", 3.9);
-        Mahasiswa27 m5 = new Mahasiswa27("127", "Miki", "2A", 3.7);
-        
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM : ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine(); 
+            
+            Mahasiswa27 m = new Mahasiswa27(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
         
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
