a551446b674b7426653805f4178b8c2a6b7acde7
jonafirman <jonafirmanmaulana01@gmail.com>
2026-04-01 10:15:58 +0700

    P6_Tugas_1&2_27

---
+++ src/P6/Tugas/Dosen27.java
@@ -0,0 +1,23 @@
+package P6.Tugas;
+
+public class Dosen27 {
+    String kode;
+    String nama;
+    Boolean jenisKelamin;
+    int usia;
+
+    public Dosen27(String kd, String name, Boolean jk, int age) {
+        kode = kd;
+        nama = name;
+        jenisKelamin = jk;
+        usia = age;
+    }
+
+    public void tampil() {
+        System.out.println("Kode Dosen    : " + kode);
+        System.out.println("Nama Dosen    : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+        System.out.println("Usia          : " + usia);
+        System.out.println("------------------------------");
+    }
+}
