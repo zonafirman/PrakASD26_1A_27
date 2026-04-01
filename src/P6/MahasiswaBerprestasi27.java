02ca7e860e41c9694e16a99cb3cb6744571c78d4
jonafirman <jonafirmanmaulana01@gmail.com>
2026-04-01 09:42:28 +0700

    P6_Percobaan2_3_27

--- src/P6/MahasiswaBerprestasi27.java
+++ src/P6/MahasiswaBerprestasi27.java
@@ -22,19 +22,17 @@ public class MahasiswaBerprestasi27 {
         }
     }
 
-    void selectionSort() {
-        for (int i = 0; i < listMhs.length - 1; i++) {
-            int idxMin = i;
-            for (int j = i + 1; j < listMhs.length; j++) {
-                if (listMhs[j] != null && listMhs[idxMin] != null) {
-                    if (listMhs[j].ipk < listMhs[idxMin].ipk) {
-                        idxMin = j;
-                    }
+    void insertionSort() {
+        for (int i = 1; i < listMhs.length; i++) {
+            if (listMhs[i] != null) {
+                Mahasiswa27 temp = listMhs[i];
+                int j = i;
+                while (j > 0 && listMhs[j - 1] != null && listMhs[j - 1].ipk > temp.ipk) {
+                    listMhs[j] = listMhs[j - 1];
+                    j--;
                 }
+                listMhs[j] = temp;
             }
-            Mahasiswa27 tmp = listMhs[idxMin];
-            listMhs[idxMin] = listMhs[i];
-            listMhs[i] = tmp;
         }
     }
     }
--- src/P6/MahasiswaDemo27.java
+++ src/P6/MahasiswaDemo27.java
@@ -1,25 +1,24 @@
 package P6;
 
 public class MahasiswaDemo27 {
-    public static void main(String[] args) {
-      MahasiswaBerprestasi27 list = new MahasiswaBerprestasi27();
-        
-       Mahasiswa27 m1 = new Mahasiswa27("123", "Ali", "2B", 3.9);
-        Mahasiswa27 m2 = new Mahasiswa27("124", "ila", "2B", 3.1);
-        Mahasiswa27 m3 = new Mahasiswa27("125", "agus", "2B", 3.6);
-        Mahasiswa27 m4 = new Mahasiswa27("126", "tika", "2B", 3.3);
-        Mahasiswa27 m5 = new Mahasiswa27("127", "udin", "2B", 3.2);
-        
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
-        System.out.println("Data Mahasiswa27 sebelum sorting: ");
-        list.tampil();
-        
-        System.out.println("Data sudah terurut menggunakan SELECTION SORT (ASC): ");
-        list.selectionSort();
-        list.tampil();
-    }
+  public static void main(String[] args) {
+    MahasiswaBerprestasi27 list = new MahasiswaBerprestasi27();
+    Mahasiswa27 m1 = new Mahasiswa27("111", "ayu", "2c", 3.7);
+    Mahasiswa27 m2 = new Mahasiswa27("222", "dika", "2c", 3.0);
+    Mahasiswa27 m3 = new Mahasiswa27("333", "ila", "2c", 3.8);
+    Mahasiswa27 m4 = new Mahasiswa27("444", "susi", "2c", 3.1);
+    Mahasiswa27 m5 = new Mahasiswa27("555", "yayuk", "2c", 3.4);
+
+    list.tambah(m1);
+    list.tambah(m2);
+    list.tambah(m3);
+    list.tambah(m4);
+    list.tambah(m5);
+    System.out.println("Data Mahasiswa27 sebelum sorting: ");
+    list.tampil();
+
+    System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+    list.insertionSort();
+    list.tampil();
+  }
 }
