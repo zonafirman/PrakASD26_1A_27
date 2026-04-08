dce61c10cdf1f9e22f41eb270ab03b006d28e39f
jonafirman <jonafirmanmaulana01@gmail.com>
2026-04-08 09:21:32 +0700

    P7_Percobaan2_PertanyaanNo5_27

--- src/P6/Percobaan2/MahasiswaBerprestasi27.java
+++ src/P6/Percobaan2/MahasiswaBerprestasi27.java
@@ -71,7 +71,7 @@ public class MahasiswaBerprestasi27 {
             if (cari == listMhs[mid].ipk){
                 return (mid);
             }
-            else if (listMhs[mid].ipk>cari) {
+            else if (listMhs[mid].ipk<cari) {
                 return findBinarySearch(cari, left, mid-1);
             }
             else {
