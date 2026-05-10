ddb7db3bcf9306306e4a4373982ce136286a6963
jonafirman <jonafirmanmaulana01@gmail.com>
2026-05-10 10:23:55 +0700

    P11_Percobaan1_Pertanyaan3_27

--- src/P11/Percobaan1/SLLMain27.java
+++ src/P11/Percobaan1/SLLMain27.java
@@ -1,13 +1,24 @@
 package P11.Percobaan1;
 
+import java.util.Scanner;
+
 public class SLLMain27 {
     public static void main(String[] args) {
         SingleLinkedList27 sll = new SingleLinkedList27();
+        Scanner sc = new Scanner(System.in);
 
-        Mahasiswa27 mhs1 = new Mahasiswa27("21212203", "Dirga", "4D", 3.6);
-        Mahasiswa27 mhs2 = new Mahasiswa27("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa27 mhs3 = new Mahasiswa27("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa27 mhs4 = new Mahasiswa27("24212200", "Alvaro", "1A", 4.0); 
+        System.out.print("Masukkan NIM: "); String nim = sc.nextLine();
+        System.out.print("Masukkan Nama: "); String nama = sc.nextLine();
+        System.out.print("Masukkan Kelas: "); String kelas = sc.nextLine();
+        System.out.print("Masukkan IPK: "); double ipk = sc.nextDouble();
+
+        Mahasiswa27 mhsBaru = new Mahasiswa27(nim, nama, kelas, ipk);
+        sll.addLast(mhsBaru);
+
+        Mahasiswa27 mhs1 = new Mahasiswa27("111", "Dirga", "1A", 3.8);
+        Mahasiswa27 mhs2 = new Mahasiswa27("112", "Budi", "1B", 3.6);
+        Mahasiswa27 mhs3 = new Mahasiswa27("113", "Tono", "1A", 3.5);
+        Mahasiswa27 mhs4 = new Mahasiswa27("114", "Rudi", "1C", 3.9);
 
         sll.print(); 
         sll.addFirst(mhs1); 
@@ -18,4 +29,5 @@ public class SLLMain27 {
         sll.insertAt(1, mhs2); 
         sll.print();
     }
+    
 }
