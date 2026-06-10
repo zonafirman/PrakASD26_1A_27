1d7f227c28da21231b162c59d948c14a9c461e8a
zonafirman <zonafirman2@email.com>
2026-06-10 08:36:28 +0700

    P15_Percobaan1_27

---
+++ src/P15/Percobaan1/Contohlist.java
@@ -0,0 +1,40 @@
+package P15.Percobaan1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class ContohList27 {
+    public static void main(String[] args) {
+        List<Object> l = new ArrayList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add("Cireng");
+        
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
+                l.get(0), l.size(), l.get(l.size() - 1));
+        
+        l.add(4);
+        l.remove(0);
+        
+        System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
+                l.get(0), l.size(), l.get(l.size() - 1));
+
+        List<String> names = new LinkedList<>();
+        names.add("Noureen");
+        names.add("Akhleema");
+        names.add("Shannum");
+        names.add("Uwais");
+        names.add("Al-Qarni");
+        
+        System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
+                names.get(0), names.size(), names.get(names.size() - 1));
+        
+        names.set(0, "My kid");
+        System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n",
+                names.get(0), names.size(), names.get(names.size() - 1));
+        
+        System.out.println("Names: " + names.toString());
+    }
+}
