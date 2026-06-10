d36dfe317ee341f9ab59d573271e2474518ddbfb
zonafirman <zonafirman2@email.com>
2026-06-10 08:42:21 +0700

    P15_Percobaan1_Pertanyaan2_27

--- src/P15/Percobaan1/Contohlist.java
+++ src/P15/Percobaan1/Contohlist.java
@@ -10,7 +10,7 @@ class ContohList27 {
         l.add(1);
         l.add(2);
         l.add(3);
-        l.add("Cireng");
+        l.add(4);
         
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
